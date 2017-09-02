2ad2abf0bfe03ebc0ec323c4aa8ae1bde86fefd1
Jswizzy <smithjustinn@hotmail.com>
2017-09-01 22:34:43 -0400

    Initial commit

---
+++ src/main/java/name/smithjn/springcloudtaskintake/SpringcloudTaskintakeApplication.java
@@ -0,0 +1,12 @@
+package name.smithjn.springcloudtaskintake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudTaskintakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudTaskintakeApplication.class, args);
+	}
+}
